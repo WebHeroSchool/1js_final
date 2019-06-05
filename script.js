9a6a302fdb648a709f80db92927a2a49a834ee7b
1Amdro <halgadam@gmail.com>
2019-06-06 00:37:46 +0300

    я изменил код, убрал var и верстку подправил

--- script.js
+++ script.js
@@ -1,64 +1,101 @@
-var menu = document.querySelector('.menu');
-var table_3 = document.querySelector('.table_3');
-var table_6 = document.querySelector('.table_6');
-var table_10 = document.querySelector('.table_10');
-var cardInner = document.querySelectorAll('.card_inner');
-var card_front = document.querySelectorAll('.card_front');
-var imagesFront = ['url(images/cardfront.png)', 'url(images/cardbug.png)'];
-var difficulty = document.querySelectorAll('.diff_variant');
-var startButton = document.querySelector('button');
-var i;
-function StartGame() {
-  if (!cardInner) {
-    throw new Error("no object found!");
+const difficulty = document.querySelectorAll('.diff_variant');
+const menu = document.querySelector('.menu');
+const tableEasy = document.querySelector('.table_easy');
+const tableNormal = document.querySelector('.table_normal');
+const tableHard = document.querySelector('.table_hard');
+const button = document.querySelector('button');
+const card = `<div class="card"> <div class="card_inner">
+  <div class="card_back"></div> <div class="card_bad"></div>
+  </div>`;
+
+function gameStart()  {
+  let cardsEasyList = document.querySelectorAll('.table_easy_row .card');
+  let cardsNormalList = document.querySelectorAll('.table_normal_rows .card');
+  let cardsHardList = document.querySelectorAll('.table_hard_rows .card');
+  let cardsList = {...cardsEasyList, ...cardsNormalList, ...cardsHardList};
+  let tableEasyRow = document.querySelector('.table_easy_row');
+  let tableNormalRows = document.querySelectorAll('.table_normal_rows');
+  let tableHardRows = document.querySelectorAll('.table_hard_rows');
+  //Выбираем сложность
+  if (difficulty[0].checked == true){
+    menu.classList.toggle('hidden');
+    tableEasy.classList.toggle('hidden');
+    //Добавляем 3 карты
+    if(cardsEasyList.length == 0){
+      for(let i = 0; i < 3;i++){
+        tableEasyRow.insertAdjacentHTML('afterBegin', card);
+      }
+    }
+  } else if (difficulty[1].checked == true){
+    menu.classList.toggle('hidden');
+    tableNormal.classList.toggle('hidden');
+    //добавляем 6 карт
+    if(cardsNormalList.length == 0){
+      tableNormalRows.forEach(function (item){
+        for(let i = 0; i < 3;i++){
+          item.insertAdjacentHTML('afterBegin', card);
+        }
+      });
+    }
+  }
+  else if (difficulty[2].checked == true){
+    menu.classList.toggle('hidden');
+    tableHard.classList.toggle('hidden');
+    //добавляем 10 карт
+    if(cardsHardList.length == 0){
+      tableHardRows.forEach(function (item){
+        for(let i = 0; i < 5;i++){
+          item.insertAdjacentHTML('afterBegin', card);
+        }
+      });
+    }
+  }
+  let cardInner = document.querySelectorAll('.card_inner');
+  reverseFlipCard(cardInner);
+  flipCard(cardInner);
+  let cardFront = document.querySelectorAll('.card_bad');
+  randomCards(cardFront);
+  returnToMenu(cardFront);
 }
-  cardInner.forEach(function(item){
-    i=0;
-    item.style.transform = 'rotateY(0deg)';
-  })
-  cardInner.forEach(function(item){
-    if (!item) {
-    throw new Error("no video object found!");
+
+function randomCards(arg){
+  const randomNames = ['card_bug', 'card_front'];
+  arg.forEach(function(item){
+    item.classList.remove('card_front');
+    item.classList.remove('card_bug');
+    item.classList.add(randomNames[Math.floor(Math.random()*2)]);
+  });
 }
+
+function flipCard(arg){
+  let i = 0;
+  arg.forEach(function (item){
     item.addEventListener('click', function f(){
       i++;
-      if(i > 1){
+      if (i > 1){
         item.removeEventListener('click', f);
-      } else{
-        item.style.transform = 'rotateY(180deg)';
+      } else {
+        item.classList.toggle('card_flip');
       }
     });
-  })
-  if(difficulty[0].checked == true){
-    menu.style.display = 'none';
-    table_3.style.display = 'block';
-    card_front.forEach(function(item){
-      item.style.backgroundImage = imagesFront[Math.floor(Math.random()*2)];
-      item.addEventListener('click', Restart);
-    })
-  } else if(difficulty[1].checked == true){
-      menu.style.display = 'none';
-      table_6.style.display = 'block';
-      card_front.forEach(function(item){
-        item.style.backgroundImage = imagesFront[Math.floor(Math.random()*2)];
-        item.addEventListener('click', Restart);
-      })
-    } else if(difficulty[2].checked == true){
-        menu.style.display = 'none';
-        table_10.style.display = 'block';
-        card_front.forEach(function(item){
-          item.style.backgroundImage = imagesFront[Math.floor(Math.random()*3)];
-          item.addEventListener('click', Restart);
-        })
-  }
+  });
 }
 
-function Restart(){
-  menu.style.display = 'flex';
-  table_3.style.display = 'none';
-  table_6.style.display = 'none';
-  table_10.style.display = 'none';
+function reverseFlipCard(arg){
+  arg.forEach(function (item){
+    item.classList.remove('card_flip');
+  });
 }
 
+function returnToMenu(arg){
+  arg.forEach(function (item){
+    item.addEventListener('click', function (){
+      menu.classList.remove('hidden');
+      tableEasy.classList.add('hidden');
+      tableNormal.classList.add('hidden');
+      tableHard.classList.add('hidden');
+    });
+  });
+}
 
-startButton.addEventListener('click', StartGame);
+button.addEventListener('click', gameStart);
